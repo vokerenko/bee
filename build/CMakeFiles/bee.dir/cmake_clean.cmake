c4cb7d8ab45c021e023e6d0950258a0cfc27fdc0
Oleh Riabov <vokerenko@gmail.com>
2025-04-08 18:58:51 +0000

    Initial upload

---
+++ build/CMakeFiles/bee.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/oleh/bee/bee"
+  "/home/oleh/bee/bee.pdb"
+  "CMakeFiles/bee.dir/src/main.c.o"
+  "CMakeFiles/bee.dir/src/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bee.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,206 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define ERROR(text) (printf("%s: %s \n", text, SDL_GetError()))
+#define YELLOW {255, 255, 0, 255}
+#define RED {255, 0, 0, 255}
+#define BLUE {0,0,255,255}
+#define GREEN {0,255,0,255}
+#define IMG_PATH "res/img/"
+#define FONT_PATH "res/font/"
+
+typedef struct {
+    float x;
+    float y;
+} Vector2;
+void normalize(Vector2* vec);
+float random_float(float min, float max);
+int collision(SDL_Rect*, SDL_Rect*);
+SDL_Texture* CreateTextureFromImage(SDL_Surface* surface, const char* image, SDL_Renderer* renderer, SDL_Window* window);
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        ERROR("SDL_Init_VIDEO");
+        return 1;
+    }
+
+    if (SDL_Init(IMG_INIT_PNG) != 0) {
+        ERROR("SDL_Init_PNG");
+        return 1;
+    }
+    if (TTF_Init() != 0) {
+        ERROR("TTF_Init()");
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Window* window = SDL_CreateWindow("Bee game", 100, 100, 900, 900, SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        ERROR("SDL_CreateWindow");
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        ERROR("SDL_CreateRenderer");
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    TTF_Font* font = TTF_OpenFont(FONT_PATH "font.ttf", 24);
+    if (!font) {
+        ERROR("font");
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    char buffer[20];
+    unsigned char running = 1;
+    srand(time(0));
+    SDL_Event event;
+    SDL_Surface* surface;
+    SDL_Rect text = {700, 50, 200, 50};
+    SDL_Texture* hive_texture = CreateTextureFromImage(surface,IMG_PATH "hive.png", renderer, window);
+    SDL_Texture* bee_texture = CreateTextureFromImage(surface, IMG_PATH "bee.png", renderer, window);
+    SDL_Texture* flower_texture = CreateTextureFromImage(surface,IMG_PATH "flower_lit.png", renderer, window);
+    SDL_Texture* flower_unlit_texture = CreateTextureFromImage(surface,IMG_PATH "flower_unlit.png", renderer, window);
+    SDL_Texture* background_texture = CreateTextureFromImage(surface,IMG_PATH "background.png", renderer, window);
+    SDL_Rect hive = {450, 450, 300, 342};
+    SDL_Rect bee = {300, 300,100, 121};
+    int has_food = 0;
+    SDL_Rect background = {0, 0, 900, 900};
+#define FLOWER_COUNT 15
+    SDL_Rect flowers[FLOWER_COUNT];
+    int is_unlit[FLOWER_COUNT];
+    float litness[FLOWER_COUNT];
+    for (int i = 0; i < FLOWER_COUNT; i++) {
+        int x, y;
+        int collides_with_flowers;
+        do {
+            collides_with_flowers = 0;
+            x = rand() % 800;
+            y = rand() % 700;
+            flowers[i].x = x;
+            flowers[i].y = y;
+            flowers[i].w = 100;
+            flowers[i].h = 117;
+            for (int j = 0; j < i; j++) {
+                if (collision(&flowers[i], &flowers[j])) {
+                    collides_with_flowers = 1;
+                }
+            }
+        } while(collision(&flowers[i], &hive) || collides_with_flowers);
+        is_unlit[i] = 0;
+        litness[i] = 1.0f;
+    }
+    float speed = 3.0f;
+    unsigned int lastTime = SDL_GetTicks();
+    float deltaTime = 0.0f;
+    while (running) {
+        if (has_food) sprintf(buffer, "has_food");
+        else sprintf(buffer, "no food");
+        SDL_Surface* text_surface = TTF_RenderText_Solid(font, buffer, (SDL_Color)RED);
+        SDL_Texture* text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+        unsigned int currentTime = SDL_GetTicks();
+        deltaTime = (currentTime - lastTime) / 1000.0f;
+        lastTime = currentTime;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+        Vector2 bee_direction = {0.0f, 0.0f};
+        const char *state = SDL_GetKeyboardState(NULL);
+        if (state[SDL_SCANCODE_LEFT]) {
+            bee_direction.x -= 1.0f;
+        }
+        if (state[SDL_SCANCODE_RIGHT]) {
+            bee_direction.x += 1.0f;
+        }
+        if (state[SDL_SCANCODE_UP]) {
+            bee_direction.y -= 1.0f;
+        }
+        if (state[SDL_SCANCODE_DOWN]) {
+            bee_direction.y += 1.0f;
+        }
+        if (state[SDL_SCANCODE_ESCAPE]) {
+            running = 0;
+        }
+        bee_direction.x += random_float(-1.0f, 1.0f);
+        bee_direction.y += random_float(-1.0f, 1.0f);
+
+        normalize(&bee_direction);
+        bee.x += roundf(speed * bee_direction.x);
+        bee.y += roundf(speed * bee_direction.y);
+        bee.x = fmax(bee.x, 0);
+        bee.y = fmin(bee.y, 900 - bee.h);
+        bee.y = fmax(bee.y, 0);
+        bee.x = fmin(bee.x, 900 - bee.w);
+        for (int i = 0; i < FLOWER_COUNT; i++) {
+            if (collision(flowers + i, &bee)) {        
+                if (!has_food && !is_unlit[i]) {
+                    litness[i] -= deltaTime; 
+                    litness[i] = fmax(litness[i], 0.0f);
+                    if (litness[i] <= 0.0f) {
+                        has_food = 1;
+                        is_unlit[i] = 1;
+                    }
+                }
+            }
+        }
+        if (collision(&hive, &bee)) {
+            has_food = 0;
+        }
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, background_texture, NULL, &background);
+        
+        for (int i = 0; i < FLOWER_COUNT; i++) {
+            SDL_RenderCopy(renderer, flower_unlit_texture, NULL, &flowers[i]);
+            SDL_SetTextureAlphaMod(flower_texture, litness[i] * 255);
+            SDL_RenderCopy(renderer, flower_texture, NULL, &flowers[i]);
+        }
+        SDL_RenderCopy(renderer, hive_texture, NULL, &hive);
+
+
+        SDL_RenderCopy(renderer, bee_texture, NULL, &bee);
+        SDL_RenderCopy(renderer, text_texture, NULL, &text);
+
+        SDL_RenderPresent(renderer);
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
+float random_float(float min, float max) {
+    return (float)rand() / (float)(RAND_MAX) * (max-min) + min;
+}
+void normalize(Vector2* vec) {
+    float length = sqrt(vec->x*vec->x +  vec->y*vec->y);
+    if (length > 0.00001f) {
+        vec->x /= length;
+        vec->y /= length;
+    }
+}
+// TODO Redo this function into few separate functions
+SDL_Texture* CreateTextureFromImage(SDL_Surface* surface, const char* image, SDL_Renderer* renderer, SDL_Window* window){
+    surface = IMG_Load(image);
+    if (!surface) {
+        SDL_Log("Image load %s  error: %s \n",image,  IMG_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return NULL;
+    }
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    return texture;
+}
+int collision(SDL_Rect* a, SDL_Rect* b) {
+    return a->x < b->x + b->w && a->x + a->w > b->x && a->y < b->y + b->h && a->y + a->h > b->y;
+}
